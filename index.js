01b1b714bfd89de6ae5fd11f2efc0d85eab5f4e1
henry.januario <primeirohenry@gmail.com>
2022-05-10 15:53:15 -0300

    JavaScript: imagem e titulo retornando

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-var btn = $('#submit')
+const btn = document.getElementById('submit')
 btn.addEventListener('click', function () {
-    apiDaNasa()    
+    apiDaNasa()
 })
 
-function apiDaNasa(){
-    var data = $('#data').val();
+function apiDaNasa() {
+    const data = $('#data').val();
 
-    
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=QecdsA0cK1vr5blGuUQkvTYa2NXkyjSYBdKty3NW&date=${data}`,
+        success: function (data) {
+            apod(data);
+        }
+
+    })
+}
+
+function apod(resultado) {
+    const imagem = $('#imagem');
+    const titulo = $('#titulo');
+    const descricao = $('descricao');
+    const assinatura = $('assinatura');
+
+    titulo.html(`${resultado.title}`);
+    descricao.html(`${resultado.explanation}`);
+    assinatura.html(`${resultado.copyright}`);
+
+    if (resultado.media_type == 'image') {
+        imagem.html(`<img class='img' src="${resultado.url}"/>`)
+    } else {
+        imagem.html(`<iframe class="img" src="${resultado.url}?autoplay=1&mute=1"></iframe>`)
+    }
 }
